9015cde15d3abab572cb91516afe8b1b63938575
Araki Takashi <ellaydelgado@gmail.com>
2024-02-07 17:25:29 +0900

    AddressControllerに全ての住所情報を返すAPIを仮実装

---
+++ src/main/java/com/examination3/address/presentation/AddressResponse.java
@@ -0,0 +1,4 @@
+package com.examination3.address.presentation;
+
+public record AddressResponse(String id, String zip_code, String prefecture, String city, String street_address) {
+}
---
+++ src/main/java/com/examination3/address/presentation/AddressResponses.java
@@ -0,0 +1,6 @@
+package com.examination3.address.presentation;
+
+import java.util.List;
+
+public record AddressResponses(List<AddressResponse> addresses) {
+}

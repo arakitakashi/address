7b5e26d52085524f2d5101b82adff66dc4eb2b02
Araki Takashi <ellaydelgado@gmail.com>
2024-02-10 05:41:51 +0900

    Addressクラスにバリデーション実装

--- src/main/java/com/examination3/address/domain/address/Address.java
+++ src/main/java/com/examination3/address/domain/address/Address.java
@@ -1,5 +1,13 @@
 package com.examination3.address.domain.address;
 
+import static com.examination3.address.domain.exception.ExceptionMessage.CITY_NOT_NULL_MESSAGE;
+import static com.examination3.address.domain.exception.ExceptionMessage.ID_NOT_NULL_MESSAGE;
+import static com.examination3.address.domain.exception.ExceptionMessage.PREFECTURE_NOT_NULL_MESSAGE;
+import static com.examination3.address.domain.exception.ExceptionMessage.STREET_ADDRESS_NOT_BLANK_MESSAGE;
+import static com.examination3.address.domain.exception.ExceptionMessage.STREET_ADDRESS_NOT_NULL_MESSAGE;
+import static com.examination3.address.domain.exception.ExceptionMessage.ZIP_CODE_NOT_NULL_MESSAGE;
+import static java.util.Objects.isNull;
+
 public record Address(
     Id id,
     ZipCode zipCode,
@@ -7,4 +15,11 @@ public record Address(
     City city,
     StreetAddress streetAddress
 ) {
+    public Address {
+        if (isNull(id)) throw new IllegalArgumentException(ID_NOT_NULL_MESSAGE.getMessage());
+        if (isNull(zipCode)) throw new IllegalArgumentException(ZIP_CODE_NOT_NULL_MESSAGE.getMessage());
+        if (isNull(prefecture)) throw new IllegalArgumentException(PREFECTURE_NOT_NULL_MESSAGE.getMessage());
+        if (isNull(city)) throw new IllegalArgumentException(CITY_NOT_NULL_MESSAGE.getMessage());
+        if (isNull(streetAddress)) throw new IllegalArgumentException(STREET_ADDRESS_NOT_NULL_MESSAGE.getMessage());
+    }
 }
--- src/main/java/com/examination3/address/domain/exception/ExceptionMessage.java
+++ src/main/java/com/examination3/address/domain/exception/ExceptionMessage.java
@@ -1,14 +1,19 @@
 package com.examination3.address.domain.exception;
 
 public enum ExceptionMessage {
-    ZIP_CODE_NOT_BLANK_MESSAGE("Error: The Zip code must not be null or blank."),
-    ZIP_CODE_7_DIGITS_MESSAGE("Error: The Zip code must be numerical and 7 digits long."),
-    PREFECTURE_NOT_BLANK_MESSAGE("Error: The Prefecture must not be null or blank."),
-    PREFECTURE_MAX_20_DIGITS_MESSAGE("Error: The Prefecture is Max 20 digits."),
-    CITY_NOT_BLANK_MESSAGE("Error: The City must not be null or blank."),
-    CITY_MAX_20_DIGITS_MESSAGE("Error: The City is Max 20 digits."),
-    STREET_ADDRESS_NOT_BLANK_MESSAGE("Error: The Street address must not be null or blank."),
-    STREET_ADDRESS_MAX_100_DIGITS_MESSAGE("Error: The Street address is Max 100 digits.");
+    ZIP_CODE_NOT_BLANK_MESSAGE("Error: Zip code must not be null or blank."),
+    ZIP_CODE_7_DIGITS_MESSAGE("Error: Zip code must be numerical and 7 digits long."),
+    PREFECTURE_NOT_BLANK_MESSAGE("Error: Prefecture must not be null or blank."),
+    PREFECTURE_MAX_20_DIGITS_MESSAGE("Error: Prefecture is Max 20 digits."),
+    CITY_NOT_BLANK_MESSAGE("Error: City must not be null or blank."),
+    CITY_MAX_20_DIGITS_MESSAGE("Error: City is Max 20 digits."),
+    STREET_ADDRESS_NOT_BLANK_MESSAGE("Error: Street address must not be null or blank."),
+    STREET_ADDRESS_MAX_100_DIGITS_MESSAGE("Error: Street address is Max 100 digits."),
+    ID_NOT_NULL_MESSAGE("Error: Id must not be null"),
+    ZIP_CODE_NOT_NULL_MESSAGE("Error: Zip code must not be null"),
+    PREFECTURE_NOT_NULL_MESSAGE("Error: Prefecture must not be null"),
+    CITY_NOT_NULL_MESSAGE("Error: City must not be null"),
+    STREET_ADDRESS_NOT_NULL_MESSAGE("Error: Street address must not be null");
 
     private final String message;
 

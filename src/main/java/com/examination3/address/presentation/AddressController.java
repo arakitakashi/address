13f68b05a40dcb564056e3a2e642a5c1ba1e80cf
Araki Takashi <ellaydelgado@gmail.com>
2024-02-12 05:43:25 +0900

    AddressContorollerにfindByIdの仮実装’

--- src/main/java/com/examination3/address/presentation/AddressController.java
+++ src/main/java/com/examination3/address/presentation/AddressController.java
@@ -1,13 +1,17 @@
 package com.examination3.address.presentation;
 
+import com.examination3.address.domain.address.Address;
 import com.examination3.address.presentation.address.AddressResponse;
 import com.examination3.address.presentation.address.AddressResponses;
 import com.examination3.address.usecase.AddressDto;
 import com.examination3.address.usecase.AddressGetAllUsecase;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +37,13 @@ public class AddressController {
     private AddressResponse convertDtoToResponse(AddressDto dto) {
         return new AddressResponse(String.valueOf(dto.id()), dto.zipCode(), dto.prefecture(),
             dto.city(), dto.streetAddress());
+    }
+
+    @GetMapping("v1/addresses/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<AddressResponse> getAddressById(@PathVariable String id) {
+        Optional<AddressDto> addressDto = Optional.of(new AddressDto(1, "1000000", "東京都", "千代田区", "以下に掲載がない場合"));
+        return addressDto.map(a -> ResponseEntity.ok(convertDtoToResponse(a)))
+            .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }

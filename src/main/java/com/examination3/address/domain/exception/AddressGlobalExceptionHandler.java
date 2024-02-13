2d009fc2423ad7956f9f2e300097e53576af9a43
Araki Takashi <ellaydelgado@gmail.com>
2024-02-14 05:14:33 +0900

    AddressGlobalExceptionHandlerにRequestの例外処理実装

---
+++ src/main/java/com/examination3/address/domain/exception/AddressGlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.examination3.address.domain.exception;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AddressGlobalExceptionHandler {
+    private static final String KEY_OF_CODE = "code";
+    private static final String KEY_OF_MESSAGE = "message";
+    private static final String KEY_OF_DETAILS = "details";
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(
+        IllegalArgumentException e
+    ) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(KEY_OF_CODE, "0002");
+        body.put(KEY_OF_MESSAGE, "request validation error is occurred");
+        List<String> details = List.of(e.getMessage());
+        body.put(KEY_OF_DETAILS, details);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/examination3/address/domain/exception/ExceptionMessage.java
+++ src/main/java/com/examination3/address/domain/exception/ExceptionMessage.java
@@ -2,19 +2,19 @@ package com.examination3.address.domain.exception;
 
 public enum ExceptionMessage {
     ADDRESS_NOT_FOUND_MESSAGE("specified address [id = %s] is not found."),
-    ZIP_CODE_NOT_BLANK_MESSAGE("Error: Zip code must not be null or blank."),
-    ZIP_CODE_7_DIGITS_MESSAGE("Error: Zip code must be numerical and 7 digits long."),
-    PREFECTURE_NOT_BLANK_MESSAGE("Error: Prefecture must not be null or blank."),
-    PREFECTURE_MAX_20_DIGITS_MESSAGE("Error: Prefecture is Max 20 digits."),
-    CITY_NOT_BLANK_MESSAGE("Error: City must not be null or blank."),
-    CITY_MAX_20_DIGITS_MESSAGE("Error: City is Max 20 digits."),
-    STREET_ADDRESS_NOT_BLANK_MESSAGE("Error: Street address must not be null or blank."),
-    STREET_ADDRESS_MAX_100_DIGITS_MESSAGE("Error: Street address is Max 100 digits."),
-    ID_NOT_NULL_MESSAGE("Error: Id must not be null"),
-    ZIP_CODE_NOT_NULL_MESSAGE("Error: Zip code must not be null"),
-    PREFECTURE_NOT_NULL_MESSAGE("Error: Prefecture must not be null"),
-    CITY_NOT_NULL_MESSAGE("Error: City must not be null"),
-    STREET_ADDRESS_NOT_NULL_MESSAGE("Error: Street address must not be null");
+    ZIP_CODE_NOT_BLANK_MESSAGE("zip code must not be blank."),
+    ZIP_CODE_7_DIGITS_MESSAGE("zip code must be numerical and 7 digits long."),
+    PREFECTURE_NOT_BLANK_MESSAGE("prefecture must not be blank."),
+    PREFECTURE_MAX_20_DIGITS_MESSAGE("prefecture is Max 20 digits."),
+    CITY_NOT_BLANK_MESSAGE("city must not be blank."),
+    CITY_MAX_20_DIGITS_MESSAGE("city is Max 20 digits."),
+    STREET_ADDRESS_NOT_BLANK_MESSAGE("street address must not be blank."),
+    STREET_ADDRESS_MAX_100_DIGITS_MESSAGE("street address is Max 100 digits."),
+    ID_NOT_NULL_MESSAGE("id must not be null."),
+    ZIP_CODE_NOT_NULL_MESSAGE("zip code must not be null."),
+    PREFECTURE_NOT_NULL_MESSAGE("prefecture must not be null."),
+    CITY_NOT_NULL_MESSAGE("city must not be null."),
+    STREET_ADDRESS_NOT_NULL_MESSAGE("street address must not be null.");
 
     private final String message;
 

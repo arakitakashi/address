768a0c02468e75b2c822fd8466d8022ce1d30269
Araki Takashi <ellaydelgado@gmail.com>
2024-02-14 05:27:18 +0900

    AddressControllerに更新APIの仮実装

--- src/main/java/com/examination3/address/presentation/AddressController.java
+++ src/main/java/com/examination3/address/presentation/AddressController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -64,5 +65,14 @@ public class AddressController {
     private URI getLocation(AddressDto addressDto) {
         return ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
             .buildAndExpand(addressDto.id()).toUri();
+    }
+
+    @PatchMapping("/v1/addresses/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> updateAddress(
+        @PathVariable String id,
+        @RequestBody AddressRequest addressRequest
+    ) {
+        return ResponseEntity.noContent().build();
     }
 }

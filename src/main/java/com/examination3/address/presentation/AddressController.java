df4faed25dc568c9f92d18e58f06554295758763
Araki Takashi <ellaydelgado@gmail.com>
2024-02-14 07:03:45 +0900

    AddressControllerに削除APIの仮実装

--- src/main/java/com/examination3/address/presentation/AddressController.java
+++ src/main/java/com/examination3/address/presentation/AddressController.java
@@ -8,11 +8,13 @@ import com.examination3.address.usecase.AddressGetAllUsecase;
 import com.examination3.address.usecase.AddressGetByIdUsecase;
 import com.examination3.address.usecase.AddressRegisterUsecase;
 import com.examination3.address.usecase.AddressUpdateUsecase;
+import java.awt.color.ICC_ColorSpace;
 import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -76,6 +78,12 @@ public class AddressController {
         @RequestBody AddressRequest addressRequest
     ) {
         addressUpdateUsecase.execute(id, addressRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/v1/addresses/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> deleteAddresses(@PathVariable String id) {
         return ResponseEntity.noContent().build();
     }
 }

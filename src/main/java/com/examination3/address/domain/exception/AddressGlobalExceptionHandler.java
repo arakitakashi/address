bc50e4c76daf401881be4c3f1f2488f0e96c0b83
Araki Takashi <ellaydelgado@gmail.com>
2024-02-14 06:52:41 +0900

    AddressGlobalExceptionHandlerにAddressNotFoundExceptionのハンドラーを実装

--- src/main/java/com/examination3/address/domain/exception/AddressGlobalExceptionHandler.java
+++ src/main/java/com/examination3/address/domain/exception/AddressGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.examination3.address.domain.exception;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +14,18 @@ public class AddressGlobalExceptionHandler {
     private static final String KEY_OF_CODE = "code";
     private static final String KEY_OF_MESSAGE = "message";
     private static final String KEY_OF_DETAILS = "details";
+
+    @ExceptionHandler(AddressNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleAddressNotFoundException(
+        AddressNotFoundException e
+    ) {
+       Map<String, Object> body = new LinkedHashMap<>();
+       body.put(KEY_OF_CODE, "0003");
+       body.put(KEY_OF_MESSAGE, e.getMessage());
+       body.put(KEY_OF_DETAILS, new ArrayList<>());
+
+       return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(

e60fd58cb151215a73ba6cb121329b995095b6b6
Araki Takashi <ellaydelgado@gmail.com>
2024-02-14 09:35:10 +0900

    AddressDeleteUsecaseの仮実装

---
+++ src/main/java/com/examination3/address/usecase/AddressDeleteUsecase.java
@@ -0,0 +1,13 @@
+package com.examination3.address.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AddressDeleteUsecase {
+    public boolean execute(String id) {
+        return true;
+
+    }
+}
